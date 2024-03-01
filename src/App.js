7ebe6a4f79d527f51620fe982286adde999b3654
Hamidah Oderinwale <oderinwaleh@gmail.com>
2024-03-01 11:44:19 -0500

    done

---
+++ src/App.js
@@ -0,0 +1,122 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Learnathon</h1>
+      <p>2/2024</p>
+      <blockquote><em>“Self-education is, I firmly believe, the only kind of education there is." <br></br>             — Isaac Asimov</em></blockquote>
+<blockquote>
+  <p class="smallerfont">I'm not that cultured, I stole the quote idea from this <a href="https://arxiv.org/abs/2310.01798" target="_blank">paper</a>.</p>
+</blockquote>
+      <p>
+        A learning sprint.{" "}
+        <a href="https://www.rabbitholeathon.com/" target="_blank">
+          Focused rabbit-holes (in a sprint?)
+        </a>
+        . Something new that's a little more familiar...
+      </p>
+      <p>
+        The Learnathon is a chance to finally dive deep into a topic that piqued
+        your curiosity, but you never pursued; a problem you’re itching to
+        solve; or an issue you’re trying to make sense of. It’s a space to
+        spend a few concerted hours exploring a new topic, subject, or idea.
+      </p>
+
+      <p>
+        Running Learnathons are really easy. If you do run one, I think it's
+        important to set the scene for what to do. An email was sent out about
+        a week in advance, so people could think about what they might do,
+        collect resources, etc.
+      </p>
+      <p>
+        Learning is ominous. Deliverables are great! A blog post, a
+        written-out-proof, some slightly less buggy code.
+      </p>
+
+<div class="wrap-around">
+      <p>
+        Whatever it is, learning by doing is supreme. But, it's also reasonable
+        and encouraged to just try and understand whatever it is you want to
+        wrap your mind around (that's what I did!). A slightly-complex research
+        paper is a great start, especially if it's related to other work you're
+        trying to accomplish.
+      </p>
+
+      <hr />
+
+      <div><img src="https://images.squarespace-cdn.com/content/v1/5a7cb549e5dd5b79d078323c/1518798993177-ZIBAZROUZ8R69LBC20NA/22041977_696733900509754_4022971350234027946_o.jpg?format=500w" alt="Building21" /></div>
+
+      <em>
+        <p>
+          <a href="https://building21.ca/" target="_blank">
+            <u>Building21</u>
+          </a>{" "}
+          graciously supported the event. The initial inspiration for running
+          this was a{" "}
+          <a href="https://firelight-mit.github.io/" target="_blank">
+            friend club</a> at his school.
+        </p>
+      </em>
+    
+
+      <p>Here's the structure we followed:</p>
+      <div>
+        <ul>
+          <li>15 minute pre-amble</li>
+          <li>1 hour work time</li>
+          <li>15 minute breaktime</li>
+          <li>1 hour work time</li>
+          <li>15+ minute debrief</li>
+        </ul>
+      </div>
+    
+      <p>In total it was 2 hours and 45 minutes.</p>
+      </div>
+
+      <p>
+        Note that the time frame is super short! It was nice. You build enough
+        momentum that you can continue if you'd like. If not, it's a nice boost
+        of motivation to do something else. But, at least you did something!
+        This is probably the most valuable takeaway. Just do something.
+      </p>
+
+      <h3>Cool stuff people learned and worked on</h3>
+
+
+      <p>
+        People did chemistry (crystallography!); first-order logic; messed
+        around with code; caught up on reading; researched thyroid imbalances,{" "}
+        <a href="archive.html">
+          <u>Archive Fever</u>
+        </a>
+        , and{" "}
+        <a href="src/projects.svelte">
+          more
+        </a>
+        .
+      </p>
+
+      <p>
+        Overall, it was really great to run (albeit I didn't have to actually
+        do that much). People are usually excited to talk about things that
+        excite them. This is a fun catalyst for great conversations too.
+      </p>
+
+      <p>
+        The most notable thing was just how the mood changed. Co-working
+        sessions are great, but I think there's this pressure to have something
+        to talk about by the end, so you just end up actually trying. There's
+        nothing concrete to display, so you just get guided ramblings (which is
+        really dope).
+      </p>
+
+      <p>In short, run one!</p>
+    </div>
+  );
+}
+
+
+export default App;
